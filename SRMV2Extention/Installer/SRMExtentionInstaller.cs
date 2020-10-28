339760a81d432ab1984dc503a3eb23a7ab420081
デンパ時計 <jsoldenpa@yahoo.co.jp>
2020-10-28 22:57:36 +0900

    プロジェクト ファイルを追加します。

---
+++ SRMV2Extention/Installer/SRMExtentionInstaller.cs
@@ -0,0 +1,19 @@
+﻿using SongRequestManagerV2.Interfaces;
+using SRMV2Extention.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Zenject;
+
+namespace SRMV2Extention.Installer
+{
+    public class SRMExtentionInstaller : MonoInstaller
+    {
+        public override void InstallBindings()
+        {
+            Container.BindInterfacesAndSelfTo<SRMExtentionCommand>().AsTransient();
+        }
+    }
+}
---
+++ SRMV2Extention/Models/SRMExtentionCommand.cs
@@ -0,0 +1,26 @@
+﻿using ChatCore.Interfaces;
+using SongRequestManagerV2.Bases;
+using SongRequestManagerV2.Interfaces;
+using SongRequestManagerV2.Models;
+using SongRequestManagerV2.Statics;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using Zenject;
+
+namespace SRMV2Extention.Models
+{
+    public class SRMExtentionCommand : SRMCommandBase
+    {
+
+        public override void Constractor()
+        {
+            Plugin.Log.Debug("Initialize!");
+            this.Setup("!denpadokei").Action((c, s) => { this._bot.QueueChatMessage($"テストだよー！ from {c.DisplayName}"); }).Help(FlagParameter.Everyone, "ショートテストです。");
+        }
+    }
+}
